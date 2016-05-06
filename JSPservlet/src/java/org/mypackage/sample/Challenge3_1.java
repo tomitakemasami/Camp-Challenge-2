a45eb46fea991bee02f6204950a260f46c8f9f95
冨武真生 <genuinelive1124@gmail.com>
2016-05-06 11:05:47 +0900

    基礎３　課題１

---
+++ JSPservlet/src/java/org/mypackage/sample/Challenge3_1.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ */
+public class Challenge3_1 extends HttpServlet {
+
+String getName() {
+    return "冨武真生";
+}
+
+String getBirth() {
+    return "１９８６年１１月２４日";
+}
+
+String getFrom() {
+    return "神奈川県大和市";
+}
+
+
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+         PrintWriter out = response.getWriter();
+
+         for(int i=0;i<10 ;i++ ) {
+     
+String name = getName();
+out.println("私の名前は"+name+"です<br>");
+
+String birth = getBirth();
+out.println("生年月日は"+birth+"です<br>");
+
+String from = getFrom();
+out.println("出身は"+from+"です<br>");
+
+out.print("<br>");
+
+ }
+         
+        try{
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Challenge3_1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>"+"</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }finally{
+    out.close();
+            }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
